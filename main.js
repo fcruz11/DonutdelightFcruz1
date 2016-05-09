df6f827c395febef717fe002dfdc78676d690519
Francisco Cruz <fcruz1@stu.parkland.edu>
2016-05-09 02:36:03 -0500

    jquery

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
 // https://learn.jquery.com/using-jquery-core/document-ready/
 $(document).ready(function(){
     
-    console.log("hello world!");
+   $("#email_form").validate({
+        rules: {
+            "firstname": {
+                required: true,
+            }, 
+            "lastname":{
+                required: true
+            },
+            "Email": {
+                required: true,
+                email: true
+            },
+            
+            
+            
+        },
+        messages: {
+            "email_1": {
+                required: "Enter your Name." 
+            },
+            "lastname":{
+                required: "Enter your Last Name."
+            },
+            "Email":{
+                required: "Enter your Email."
+            }
+        }
+        
+    });
 
 });
